03361ea75b8773769072698498720c5189a2b4e5
MING-CHANG LEE     (李明昌) <36437869+rwepa@users.noreply.github.com>
2023-04-11 20:21:30 +0800

    Create 10_pandas_visualization.py

---
+++ 10_pandas_visualization.py
@@ -0,0 +1,65 @@
+"""
+File     : 10_pandas_visualization.py
+Title    : 第10章 pandas 視覺化
+Author   : Ming-Chang Lee
+YouTube  : https://www.youtube.com/@alan9956
+RWEPA    : http://rwepa.blogspot.tw/
+GitHub   : https://github.com/rwepa
+Email    : alan9956@gmail.com
+"""
+
+##############################
+# 大綱
+##############################
+
+# 10.1 pandas 視覺化簡介
+# 10.2 pandas 直方圖
+# 10.3 pandas 散佈圖
+# 10.4 pandas 長條圖
+
+##############################
+# 10.1 pandas 視覺化簡介
+##############################
+
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.html
+
+# pandas.DataFrame.plot.area    區域圖
+# pandas.DataFrame.plot.bar     長條圖
+# pandas.DataFrame.plot.barh    水平長條圖
+# pandas.DataFrame.plot.box     盒鬚圖
+# pandas.DataFrame.plot.density 密度圖
+# pandas.DataFrame.plot.hexbin  六邊分箱圖(hexagonal binning plot)
+# pandas.DataFrame.plot.hist    直方圖
+# pandas.DataFrame.plot.kde     核密度圖
+# pandas.DataFrame.plot.line    線圖
+# pandas.DataFrame.plot.pie     圓形圖
+# pandas.DataFrame.plot.scatter 散佈圖
+# pandas.DataFrame.boxplot      盒鬚圖
+
+# 使用 human_resource.csv 資料集繪製以下圖形並說明繪圖結果
+# 人力資源資料集說明: https://github.com/rwepa/DataDemo#human_resourcecsv
+# 人力資源資料集下載: https://github.com/rwepa/DataDemo/blob/master/human_resource.csv
+
+##############################
+# 10.2 pandas 直方圖
+##############################
+
+# pandas.DataFrame.plot.hist
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.hist.html
+
+##############################
+# 10.3 pandas 散佈圖
+##############################
+
+# pandas.DataFrame.plot.scatter
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.scatter.html
+
+##############################
+# 10.4 pandas 水平長條圖
+##############################
+
+# pandas.DataFrame.plot.barh
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.plot.barh.html
+
+
+# end
